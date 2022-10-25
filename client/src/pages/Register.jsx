307869f25a24fc5580e7db0df095243a5c5c27a6
Nazmul Hasan <nazmulhasan02901@gmail.com>
2022-10-25 12:03:21 +0000

    register-page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,22 +1,87 @@
 import styled from "styled-components";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function Register() {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  console.log(user);
+
+  // methods ======================================
+  const submitHandle = (e) => {
+    e.preventDefault();
+    handleValidation();
+  };
+
+  const handleValidation = () => {
+    const { name, email, password, confirmPassword } = user;
+    if (password !== confirmPassword || password.length < 8) {
+      toast.error("Password need to be matched and contained more than 7 characters");
+      return false;
+    } else if (name.length < 4) {
+      toast.error("Name should contain at least 4 characters");
+      return false;
+    } else if (email.length < 8) {
+      toast.error("Email should contain at least 8 characters");
+      return false;
+    }
+  };
+
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
   return (
     <div>
       <FormContainer>
-        <form action="">
+        <form action="" onSubmit={submitHandle}>
           <div className="brand">
-            <img src="/logo192.png" alt="" width={50} height={50}/>
+            <img src="/logo192.png" alt="" width={50} height={50} />
             <h1>Msg</h1>
           </div>
-          <input type="text" name="text" placeholder="name"/>
-          <input type="email" placeholder="email" name="email" />
-          <input type="password" name="password" placeholder="password"/>
-          <input type="password" name="confirmPassword" placeholder="confirm password"/>
-          <button type="submit">Submit</button>
-          <span>Already have an account ? <Link to="/login">Login</Link></span>
+          <input
+            onChange={handleChange}
+            value={user.name}
+            type="text"
+            name="name"
+            placeholder="name"
+            required
+          />
+          <input
+            onChange={handleChange}
+            value={user.email}
+            type="email"
+            placeholder="email"
+            name="email"
+            required
+          />
+          <input
+            onChange={handleChange}
+            value={user.password}
+            type="password"
+            name="password"
+            placeholder="password"
+          />
+          <input
+            onChange={handleChange}
+            value={user.confirmPassword}
+            type="password"
+            name="confirmPassword"
+            placeholder="confirm password"
+          />
+          <button type="submit">Create User</button>
+          <span>
+            Already have an account ? <Link to="/login">Login</Link>
+          </span>
         </form>
       </FormContainer>
+      <ToastContainer position= "bottom-right" />
     </div>
   );
 }
@@ -26,40 +91,44 @@ const FormContainer = styled.div`
   justify-content: center;
   align-items: center;
   height: 100vh;
-  form{
-    display:flex;
+  form {
+    display: flex;
     flex-direction: column;
-    justify-content:center;
-    align-items:center;
+    justify-content: center;
+    align-items: center;
     gap: 2rem;
     background-color: #00000076;
     padding: 3rem 5rem;
     border-radius: 2rem;
   }
-  .brand{
+  .brand {
     display: flex;
-    align-items:center;
+    align-items: center;
     justify-content: center;
     gap: 1rem;
   }
-  input{
+  input {
     padding: 1rem;
     border-radius: 0.4rem;
     border: none;
     font-size: 1rem;
     width: 100%;
-    &:focus{
+    &:focus {
       outline: none;
     }
   }
-  button{
+  button {
     padding: 1rem;
     border-radius: 0.4rem;
     width: 100%;
     font-size: 1.2rem;
     cursor: pointer;
     border: none;
+    &:hover {
+      background-color: dodgerblue;
+      color: white;
+    }
+    transition: all 0.3s ease;
   }
-
 `;
 export default Register;
